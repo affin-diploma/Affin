70df46cfd0033a6879fae9c5c4a8563a03984288
affin-diploma <82399152+affin-diploma@users.noreply.github.com>
2021-05-25 01:11:05 +0300

    debug on

--- afin/settings.py
+++ afin/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2g$#_&ag(nl-bs=^qgpnx@-%ad3#ok8jjgg&f%n=lf7^lupfu_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
